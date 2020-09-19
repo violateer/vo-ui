aed7e19c2bf940cf772b947a2d57c2b0a80549dc
violateer <1828257089@qq.com>
2020-09-19 18:51:10 +0800

    按钮hover效果

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,8 @@
 <template>
 <div>
-    <button><span></span></button>
+    <button>
+        <span></span>
+    </button>
 </div>
 </template>
 
@@ -30,6 +32,10 @@ span {
     height: $h2;
     width: $h2;
     background: white;
-    border-radius: $h2/2;
+    border-radius: $h2 / 2;
+}
+
+button:hover span {
+    left: calc(100% - #{$h2} - 2px)
 }
 </style>
