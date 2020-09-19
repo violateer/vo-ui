7f84c7013d6d5313b035155d005bc464b4f544df
violateer <1828257089@qq.com>
2020-09-19 14:51:53 +0800

    初始化aside的html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,13 +3,29 @@
     <Topnav />
     <div class="content">
         <aside>边栏</aside>
-        <main>主内容</main>
+        <aside>
+            <h2>组件列表</h2>
+            <ol>
+                <li>
+                    <router-link to="/doc/switch">Switch 组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/button">Button 组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/dialog">Dialog 组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/tabs">Tabs 组件</router-link>
+                </li>
+            </ol>
+        </aside>
     </div>
 </div>
 </template>
 
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue"
 export default {
     components: {
         Topnav
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
         <h1>VO-UI</h1>
         <h2>一个厉害的UI框架</h2>
         <p class="actions">
-            <a>Github</a>
-            <a>开始</a>
+            <a href="https://github.com/violateer/vo-ui">Github</a>
+            <router-link to="/doc">开始</router-link>
         </p>
     </div>
 </div>
