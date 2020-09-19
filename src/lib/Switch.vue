1711e13a569543f448de2ec16a9b2f5959cd0d35
violateer <1828257089@qq.com>
2020-09-19 20:49:37 +0800

    优化代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -36,28 +36,28 @@ button {
     background: grey;
     border-radius: $h/2;
     position: relative;
-}
 
-span {
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background: white;
-    border-radius: $h2 / 2;
-    transition: left 250ms;
-}
+    >span {
+        position: absolute;
+        top: 2px;
+        left: 2px;
+        height: $h2;
+        width: $h2;
+        background: white;
+        border-radius: $h2 / 2;
+        transition: left 250ms;
+    }
 
-button.checked {
-    background: blue;
-}
+    &.checked {
+        background: blue;
 
-button.checked span {
-    left: calc(100% - #{$h2} - 2px)
-}
+        >span {
+            left: calc(100% - #{$h2} - 2px)
+        }
+    }
 
-button:focus {
-    outline: 0;
+    &:focus {
+        outline: 0;
+    }
 }
 </style>
