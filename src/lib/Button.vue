47fd6b66de8615b596ff6b031b7b949ab2a00291
violateer <1828257089@qq.com>
2020-09-20 10:11:54 +0800

    Button基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,29 +1,58 @@
 <template>
-<div>
-    <button v-bind="rest">
-        <slot />
-    </button>
-</div>
+<button class="vo-button" :class="{[`theme-${theme}`]: theme}">
+    <slot />
+</button>
 </template>
 
 <script lang="ts">
 export default {
-    inheritAttrs: false,
-    setup(props, context) {
-        const {
-            size,
-            ...rest
-        } = context.attrs
-        return {
-            size,
-            rest
+    props: {
+        theme: {
+            type: String,
+            default: 'button'
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-div {
-    border: 1px solid red;
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+
+.vo-button {
+    box-sizing: border-box;
+    height: $h;
+    padding: 0 12px;
+    cursor: pointer;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    background: white;
+    color: $color;
+    border: 1px solid $border-color;
+    border-radius: $radius;
+    box-shadow: 0 1px 0 fade-out(black, 0.95);
+
+    &+& {
+        margin-left: 8px;
+    }
+
+    &:hover,
+    &:focus {
+        color: $blue;
+        border-color: $blue;
+    }
+
+    &:focus {
+        outline: none;
+    }
+
+    &::-moz-focus-inner {
+        border: 0;
+    }
 }
 </style>
