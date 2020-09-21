8a41f64a5830650d4817a1b227d473a33c52a0c9
violateer <1828257089@qq.com>
2020-09-21 12:09:35 +0800

    渲染Tabs内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,26 @@
 <template>
 <div>
-    Tabs组件
+    <div v-for="(title, index) in titles" :key="index">{{title}}</div>
+    <component v-for="(c, index) in defaults" :is="c" :key="index" />
 </div>
 </template>
 
-<script>
+<script lang="ts">
+import Tab from "./Tab.vue"
 export default {
-
+    setup(props, context) {
+        const defaults = context.slots.default()
+        defaults.forEach(tag => {
+            if (tag.type !== Tab) {
+                throw new Error('Tabs子标签必须是Tab')
+            }
+        })
+        const titles = defaults.map(tag => tag.props.title)
+        return {
+            defaults,
+            titles
+        }
+    }
 }
 </script>
 
