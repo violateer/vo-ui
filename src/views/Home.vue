10d17d07128fa2bb8503e0c542e4b936cd688e68
violateer <1828257089@qq.com>
2020-09-22 20:40:38 +0800

    添加图标

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-<div class="topnavAndBanner">
-    <Topnav />
-    <div class="banner">
-        <h1>VO-UI</h1>
-        <h2>一个厉害的UI框架</h2>
-        <p class="actions">
-            <a href="https://github.com/violateer/vo-ui">Github</a>
-            <router-link to="/doc">开始</router-link>
-        </p>
+<div>
+    <div class="topnavAndBanner">
+        <Topnav />
+        <div class="banner">
+            <h1>VO-UI</h1>
+            <h2>一个厉害的UI框架</h2>
+            <p class="actions">
+                <a href="https://github.com/violateer/vo-ui">Github</a>
+                <router-link to="/doc">开始</router-link>
+            </p>
+        </div>
+    </div>
+    <div class="features">
+        <svg class="icon">
+            <use xlink:href="#icon-ts"></use>
+        </svg>
+        <svg class="icon">
+            <use xlink:href="#icon-Vue"></use>
+        </svg>
+        <svg class="icon">
+            <use xlink:href="#icon-dengpao"></use>
+        </svg>
     </div>
 </div>
 </template>
@@ -29,6 +42,13 @@ $color: #007974;
 .topnavAndBanner {
     background: rgb(227, 255, 253);
     background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+}
+
+.features {
+    >svg {
+        width: 64px;
+        height: 64px;
+    }
 }
 
 .banner {
