812bff02e71755d718521ced12f6829959ccde7a
violateer <1828257089@qq.com>
2020-09-22 23:20:51 +0800

    实现复杂一点的高亮

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -58,10 +58,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.router-link-active {
-    color: red;
-}
-
 .layout {
     display: flex;
     flex-direction: column;
@@ -99,7 +95,7 @@ export default {
 aside {
     background: lightblue;
     width: 150px;
-    padding: 16px;
+    padding: 16px 0;
     position: fixed;
     top: 0;
     left: 0;
@@ -109,11 +105,19 @@ aside {
 
     >h2 {
         margin-bottom: 4px;
+        padding: 0 16px;
     }
 
     >ol {
         >li {
-            padding: 4px 0;
+            >a {
+                display: block;
+                padding: 4px 16px;
+            }
+
+            .router-link-active {
+                background: white;
+            }
         }
     }
 }
