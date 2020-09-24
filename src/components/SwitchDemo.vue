6665c9e64915d228882e485917cae0c1ffed7c41
violateer <1828257089@qq.com>
2020-09-24 09:11:05 +0800

    less重写SwitchDemo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -23,17 +23,17 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-$border-color: #d9d9d9;
+<style lang="less" scoped>
+@border-color: #d9d9d9;
 
 .demo {
-    border: 1px solid $border-color;
+    border: 1px solid @border-color;
     margin: 16px 0 32px;
 
-    >h2 {
+    h2 {
         font-size: 20px;
         padding: 8px 16px;
-        border-bottom: 1px solid $border-color;
+        border-bottom: 1px solid @border-color;
     }
 
     &-component {
@@ -42,14 +42,14 @@ $border-color: #d9d9d9;
 
     &-actions {
         padding: 8px 16px;
-        border-top: 1px dashed $border-color;
+        border-top: 1px dashed @border-color;
     }
 
     &-code {
         padding: 8px 16px;
-        border-top: 1px dashed $border-color;
+        border-top: 1px dashed @border-color;
 
-        >pre {
+        pre {
             line-height: 1.1;
             font-family: Consolas, 'Courier New', Courier, monospace;
             margin: 0;
