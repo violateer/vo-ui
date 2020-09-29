7b2b2a9f84bcf59ab71e2239b354aa24026cb738
violateer <1828257089@qq.com>
2020-09-29 21:27:48 +0800

    修改indicator样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -107,7 +107,7 @@ $border-color: #d9d9d9;
             background: $purple;
             left: 0;
             bottom: -1px;
-            width: 100px;
+            width: 0;
             transition: all 250ms;
         }
     }
